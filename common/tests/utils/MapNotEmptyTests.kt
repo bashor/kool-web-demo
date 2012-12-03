8995b3ac885bf014e81b80875a7a418bd071597d
bashor <bashorov@gmail.com>
2012-12-03 16:20:46 +0400

    add tests Map.NotEmpty()

---
+++ common/tests/utils/MapNotEmptyTests.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2000-2012 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.webdemo.common.utils
+
+import org.junit.Test as test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import kotlin.test.assertTrue
+import kotlin.test.assertFalse
+
+RunWith(javaClass<JUnit4>())
+public class MapNotEmptyTests {
+    test fun `for empty map`() {
+        assertFalse(hashMap<Any, Any>().notEmpty())
+    }
+
+    test fun `for nonempty map`() {
+        assertTrue(hashMap(1 to 2).notEmpty())
+    }
+}

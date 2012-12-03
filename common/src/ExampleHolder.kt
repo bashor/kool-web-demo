e6c82e4b2be01d2e6e63c148db42797bf5e68cf9
bashor <bashorov@gmail.com>
2012-12-03 19:47:17 +0400

    add tests for ExampleHolder#toJsonString

--- common/src/ExampleHolder.kt
+++ common/src/ExampleHolder.kt
@@ -35,7 +35,7 @@ inline fun ExampleHolder.toJsonString(): String {
 
     result.put(NAME_PROP, this.name)
     result.put(TEXT_PROP, this.text)
-    result.put(TARGET_PROP, this.targets.toList().map { it.toString().toLowerCase() }.makeString(" "))
+    result.put(TARGET_PROP, this.targets.toSortedSet().map { it.toString().toLowerCase() }.makeString(" "))
     result.put(ARGS_PROP, this.args)
     result.put(SOURCE_PROP, this.source)
 
